f4487f285f3b2a1ca371cf752b42df6adf8a9d7f
thuan <devthuan24@gmail.com>
2022-10-22 14:08:08 +0700

    second commit

--- class.py
+++ class.py
@@ -1,13 +1,17 @@
 
-def Video():
+def Student():
     def __init__(self):
         pass
 
 def get_data():
-    pass
+    name = input("Enter your name: ")
+    id_stud = input("Enter your id_stud: ")
+    major = input("Enter your major : ")
+    student = Student(name, id_stud, major)
+    
 
 def print_info():
-    pass
+    print()
 
 
 def main():
