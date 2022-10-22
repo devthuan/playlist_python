3c62c5ceb238c79e6b0c110f231d76077a718f8f
thuan <devthuan24@gmail.com>
2022-10-22 14:13:43 +0700

    complete get data student

--- class.py
+++ class.py
@@ -1,20 +1,32 @@
 
-def Student():
-    def __init__(self):
-        pass
+from unicodedata import name
+
+
+class Student():
+    def __init__(self, name, id_stud, major):
+        self.name = name
+        self.id_stud = id_stud
+        self.major = major
 
 def get_data():
     name = input("Enter your name: ")
     id_stud = input("Enter your id_stud: ")
     major = input("Enter your major : ")
     student = Student(name, id_stud, major)
+
+    return student
     
 
-def print_info():
-    print()
+def print_info(data):
+    print("Name: ", data.name)
+    print("ID student: ", data.id_stud)
+    print("major: ", data.major)
+
 
 
 def main():
-    pass
+    stud = get_data()
+
+    print_info(stud)
 
 main()
